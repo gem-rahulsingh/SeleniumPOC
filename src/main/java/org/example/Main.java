33e860c1d25211c60538fcde448e58a8646d156c
Rahul Kumar Singh <rahul.singh@Geminisolutions.com>
2023-10-17 13:12:10 +0530

    First

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class Main {
+    public static void main(String[] args) {
+        // Setup WebDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to a website
+        driver.get("https://example.com");
+
+        // Perform Selenium actions here...
+
+        // Close the browser
+        driver.quit();
+    }
+}
