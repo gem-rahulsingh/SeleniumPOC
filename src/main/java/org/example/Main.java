c349c37233944596eaf2faf7cfe6916f620c7117
gem-rahulsingh <103640876+gem-rahulsingh@users.noreply.github.com>
2023-10-17 13:41:29 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,24 @@
 package org.example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 public class Main {
     public static void main(String[] args) {
-        // Setup WebDriver using WebDriverManager
-        WebDriverManager.chromedriver().setup();
+        // // Setup WebDriver using WebDriverManager
+        // WebDriverManager.chromedriver().setup();
 
-        // Initialize ChromeDriver
-        WebDriver driver = new ChromeDriver();
+        // // Initialize ChromeDriver
+        // WebDriver driver = new ChromeDriver();
 
-        // Navigate to a website
-        driver.get("https://example.com");
+        // // Navigate to a website
+        // driver.get("https://example.com");
 
-        // Perform Selenium actions here...
+        // // Perform Selenium actions here...
 
-        // Close the browser
-        driver.quit();
+        // // Close the browser
+        // driver.quit();
+        System.out.println("Hello World");
     }
 }
