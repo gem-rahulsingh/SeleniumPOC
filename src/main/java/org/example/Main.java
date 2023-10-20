da647e63df9dc3717fbb4462ba8ed49de8828201
gem-rahulsingh <103640876+gem-rahulsingh@users.noreply.github.com>
2023-10-20 19:15:44 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,9 @@
-package org.example;
+import org.junit.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+public class MyTest {
 
-public class Main {
-
-    public static void main(String[] args) {
-        // Setup Chrome WebDriver using WebDriverManager
-        WebDriverManager.chromedriver().setup();
-
-        // Instantiate ChromeDriver
-        WebDriver driver = new ChromeDriver();
-
-        // Navigate to a website
-        driver.get("https://www.example.com");
-
-        System.out.println("Hello Tesssssssssssssssssssssssssssssssssst");
-        // Perform operations on the website
-        // ...
-
-        // Close the browser
-        driver.quit();
+    @Test
+    public void testSomething() {
+        System.out.println("Hello World");
     }
 }
